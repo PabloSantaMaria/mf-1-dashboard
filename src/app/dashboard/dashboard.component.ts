1beb0812cc936ffbfa64f0d7619c06bb576c292f
PabloSantaMaria <pabloj.santamaria@gmail.com>
2024-11-22 10:46:44 -0300

    chore: add padding

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,19 +1,18 @@
-import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit, ViewChild } from '@angular/core';
-import { ApexAxisChartSeries, ApexOptions, ChartComponent, NgApexchartsModule } from "ng-apexcharts";
-import { UntypedFormArray, UntypedFormControl, UntypedFormGroup } from '@angular/forms';
-import { SeriesPipe } from "./series.pipe";
-import { SharedServiceProxy } from '../services/proxy.service';
+import {CommonModule} from '@angular/common';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {ChartComponent, NgApexchartsModule,} from 'ng-apexcharts';
+import {UntypedFormArray, UntypedFormControl, UntypedFormGroup,} from '@angular/forms';
+import {SeriesPipe} from './series.pipe';
 
 @Component({
   standalone: true,
   imports: [CommonModule, NgApexchartsModule, SeriesPipe],
   selector: 'app-dashboard',
-  templateUrl: 'dashboard.component.html'
+  templateUrl: 'dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
 })
-
 export class DashboardComponent implements OnInit {
-  @ViewChild("chart", { static: true }) chart: ChartComponent;
+  @ViewChild('chart', { static: true }) chart: ChartComponent;
 
   // private service = inject(SharedServiceProxy).getService();
 
@@ -23,13 +22,13 @@ export class DashboardComponent implements OnInit {
     // this.service.then(v => console.log({v}));
 
     this.form = new UntypedFormGroup({
-      title: new UntypedFormControl("Basic Chart"),
-      type: new UntypedFormControl("line"),
+      title: new UntypedFormControl('Basic Chart'),
+      type: new UntypedFormControl('line'),
       height: new UntypedFormControl(350),
       series: new UntypedFormArray([
         new UntypedFormGroup({
-          name: new UntypedFormControl("Series"),
-          type: new UntypedFormControl("line"),
+          name: new UntypedFormControl('Series'),
+          type: new UntypedFormControl('line'),
           data: new UntypedFormArray([
             new UntypedFormControl(this.getRandomArbitrary(0, 100)),
             new UntypedFormControl(this.getRandomArbitrary(0, 100)),
@@ -39,30 +38,30 @@ export class DashboardComponent implements OnInit {
         }),
       ]),
       xaxis: new UntypedFormArray([
-        new UntypedFormControl("Jan"),
-        new UntypedFormControl("Feb"),
-        new UntypedFormControl("Mar"),
-        new UntypedFormControl("Apr"),
+        new UntypedFormControl('Jan'),
+        new UntypedFormControl('Feb'),
+        new UntypedFormControl('Mar'),
+        new UntypedFormControl('Apr'),
       ]),
     });
   }
 
   addValue() {
-    (<UntypedFormArray>this.form.get("series")).controls.forEach((c) => {
-      (<UntypedFormArray>c.get("data")).push(
+    (<UntypedFormArray>this.form.get('series')).controls.forEach((c) => {
+      (<UntypedFormArray>c.get('data')).push(
         new UntypedFormControl(this.getRandomArbitrary(0, 100))
       );
     });
-    (<UntypedFormArray>this.form.get("xaxis")).push(
-      new UntypedFormControl("Jan")
+    (<UntypedFormArray>this.form.get('xaxis')).push(
+      new UntypedFormControl('Jan')
     );
   }
 
   addSeries() {
-    (<UntypedFormArray>this.form.get("series")).push(
+    (<UntypedFormArray>this.form.get('series')).push(
       new UntypedFormGroup({
-        name: new UntypedFormControl("Series"),
-        type: new UntypedFormControl("line"),
+        name: new UntypedFormControl('Series'),
+        type: new UntypedFormControl('line'),
         data: new UntypedFormArray([
           new UntypedFormControl(this.getRandomArbitrary(0, 100)),
           new UntypedFormControl(this.getRandomArbitrary(0, 100)),
@@ -74,11 +73,10 @@ export class DashboardComponent implements OnInit {
   }
 
   onChartReady(event: any) {
-    console.log("Chart ready", event);
+    console.log('Chart ready', event);
   }
 
   private getRandomArbitrary(min: number, max: number) {
     return Math.random() * (max - min) + min;
   }
-
 }
