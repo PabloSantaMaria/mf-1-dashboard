56a7c2e0d191fbc21fd3e58a92ad1f6cc5686d3d
alesenestrari <alesenestrari@outlook.com>
2024-11-25 10:33:26 -0300

    add tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,9 @@
-import('./bootstrap')
+// import('./bootstrap')
+// 	.catch(err => console.error(err));
+
+import { loadManifest } from '@angular-architects/module-federation';
+
+loadManifest('mf.manifest.json')
+	.catch(err => console.error(err))
+	.then(_ => import('./bootstrap'))
 	.catch(err => console.error(err));
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,22 @@
-const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+const { withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 
 module.exports = withModuleFederationPlugin({
 
   name: 'mf-latest',
 
-  // remotes: {
-  //   cppArandano: "http://localhost:4200/remoteEntry.js", // Update URL as necessary
-  // },
-
   exposes: {
     './routes': './src/app/app.routes.ts',
+    './tailwind-styles': './src/styles.css',
   },
 
-  // shared: {
-  //   ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
-  // },
-
   shared: {
-    '@angular/core': { singleton: false, strictVersion: false },
-    '@angular/common': { singleton: false, strictVersion: false },
-    '@angular/router': { singleton: false, strictVersion: false },
-    '@angular/platform-browser': { singleton: false, strictVersion: false },
-    '@angular/animations': { singleton: false, strictVersion: false },
-    'ng-apexcharts': { singleton: false, strictVersion: false },
-    apexcharts: { singleton: false, strictVersion: false },
-    rxjs: { singleton: false, strictVersion: false },
-
-    // '@angular/core': { singleton: false, strictVersion: false, requiredVersion: '^14.3.0' },
-    // '@angular/common': { singleton: false, strictVersion: false, requiredVersion: '^14.3.0' },
-    // '@angular/router': { singleton: false, strictVersion: false, requiredVersion: '^14.3.0' },
-    // '@angular/http': { singleton: false, strictVersion: false },
-    // 'ng-apexcharts': { singleton: false, strictVersion: false },
-    // apexcharts: { singleton: false, strictVersion: false },
-    // rxjs: { singleton: false, strictVersion: false }
+    '@angular/core': { singleton: true, strictVersion: false },
+    '@angular/common': { singleton: true, strictVersion: false },
+    '@angular/router': { singleton: true, strictVersion: false },
+    '@angular/forms': { singleton: false, strictVersion: false },
+    autoprefixer: { singleton: true, strictVersion: false },
+    postcss: { singleton: true, strictVersion: false },
+    tailwindcss: { singleton: true, strictVersion: false }
   }
 
 });
